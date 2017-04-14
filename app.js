bc50cb258ee0ec2aee4fbe5d88d14b46f56339db
Nattarat <ning.nattarat@gmail.com>
2017-04-14 09:18:15 +0700

    Remove call send msg

--- app.js
+++ app.js
@@ -18,22 +18,6 @@ app.get("/webhook", function (req, res) {
   if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
     console.log("Verified webhook");
     res.status(200).send(req.query["hub.challenge"]);
-	
-	if (req.body.object == "page") {
-		// Iterate over each entry
-		// There may be multiple entries if batched
-		req.body.entry.forEach(function(entry) {
-		  // Iterate over each messaging event
-		  entry.messaging.forEach(function(event) {
-			if (event.postback) {
-			  processPostback(event);
-			}
-		  });
-		});
-
-		res.sendStatus(200);
-	  }
-
   } else {
     console.error("Verification failed. The tokens do not match.");
     res.sendStatus(403);
