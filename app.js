293b4e9dc776c48bddcd43c9610fc3cfdbb80a6f
Nattarat <ning.nattarat@gmail.com>
2017-04-14 09:24:31 +0700

    Add send msg callback

--- app.js
+++ app.js
@@ -24,6 +24,25 @@ app.get("/webhook", function (req, res) {
   }
 });
 
+// All callbacks for Messenger will be POST-ed here
+app.post("/webhook", function (req, res) {
+  // Make sure this is a page subscription
+  if (req.body.object == "page") {
+    // Iterate over each entry
+    // There may be multiple entries if batched
+    req.body.entry.forEach(function(entry) {
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.postback) {
+          processPostback(event);
+        }
+      });
+    });
+
+    res.sendStatus(200);
+  }
+});
+
 function processPostback(event) {
   var senderId = event.sender.id;
   var payload = event.postback.payload;
